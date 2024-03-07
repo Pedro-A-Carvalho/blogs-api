99c48df01338fc2ccf1eaa3ad59b3e7fec63b122
Pedro Almeida Carvalho <cont.pacarvalho@gmail.com>
2024-03-07 16:51:48 -0300

    req 6

---
+++ src/models/Category.js
@@ -0,0 +1,17 @@
+const categoryModel = (sequelize, DataTypes) => {
+    const Category = sequelize.define('Category', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: DataTypes.STRING,
+    }, {
+        tableName: 'categories',
+        timestamps: false,
+    });
+  
+    return Category;
+  };
+  
+  module.exports = categoryModel;
